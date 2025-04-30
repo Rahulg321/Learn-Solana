57f3a0c801d2ec53702bdd22a7bc28f4e0387a76
Rahul Gupta <rg5353070@gmail.com>
2025-04-30 21:40:22 +0530

    Initial Commit

---
+++ client-frontend/index.ts
@@ -0,0 +1,5 @@
+import { PublicKey, TransactionInstruction } from "@solana/web3.js";
+
+new TransactionInstruction({
+  programId: PublicKey,
+});
---
+++ client-frontend/send-ping-transaction.ts
@@ -0,0 +1,54 @@
+import * as web3 from "@solana/web3.js";
+import "dotenv/config";
+import {
+  getKeypairFromEnvironment,
+  airdropIfRequired,
+} from "@solana-developers/helpers";
+
+const PING_PROGRAM_ADDRESS = "ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa";
+const PING_PROGRAM_DATA_ADDRESS =
+  "Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod";
+
+const payer = getKeypairFromEnvironment("SECRET_KEY");
+const connection = new web3.Connection(web3.clusterApiUrl("testnet"));
+
+const newBalance = await airdropIfRequired(
+  connection,
+  payer.publicKey,
+  1 * web3.LAMPORTS_PER_SOL,
+  0.5 * web3.LAMPORTS_PER_SOL
+);
+
+const transaction = new web3.Transaction();
+
+const pingProgramId = new web3.PublicKey(PING_PROGRAM_ADDRESS);
+const pingProgramDataId = new web3.PublicKey(PING_PROGRAM_DATA_ADDRESS);
+
+const instruction = new web3.TransactionInstruction({
+  programId: pingProgramId,
+  keys: [
+    {
+      pubkey: pingProgramDataId,
+      isSigner: false, // Data account is not a signer
+      isWritable: true, // Data account is modified
+    },
+  ],
+});
+
+transaction.add(instruction);
+
+try {
+  const signature = await web3.sendAndConfirmTransaction(
+    connection,
+    transaction,
+    [payer]
+  );
+  console.log("signature", signature);
+} catch (error) {
+  if (error instanceof web3.SendTransactionError) {
+    console.error("SendTransactionError:", error.message);
+    console.log(error.getLogs(connection));
+  }
+
+  console.log("an error occured while signing trans", error);
+}
